d82d1784acc997c4413dc348824480a0cbe26c4b
Bartosz Malecki <bartoszmalecki90@gmail.com>
2023-01-23 12:53:14 +0100

    Detect collision with wall.

--- scoreboard.py
+++ scoreboard.py
@@ -2,19 +2,23 @@
 from turtle import Turtle
 
 ALIGMENT = "center"
-FONT = ('Arial', 16, 'normal')
+FONT = ('Arial', 20, 'normal')
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.sety(270)
+        self.sety(260)
         self.hideturtle()
         self.color("white")
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.write(f"SCORE: {self.score}", align=ALIGMENT,font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align=ALIGMENT,font=FONT)
 
     def increase_score(self):
         self.clear()
