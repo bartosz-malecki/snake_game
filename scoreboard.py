b825285b42fc10f8e8effba7fb3850590265df84
Bartosz Malecki <bartoszmalecki90@gmail.com>
2023-02-09 12:59:08 +0100

    Added keeping high score possibility.

--- scoreboard.py
+++ scoreboard.py
@@ -3,25 +3,34 @@ from turtle import Turtle
 
 ALIGMENT = "center"
 FONT = ('Arial', 20, 'normal')
+
+
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.sety(260)
         self.hideturtle()
         self.color("white")
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"SCORE: {self.score}", align=ALIGMENT,font=FONT)
+        self.clear()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGMENT,font=FONT)
+        self.write(f"Score: {self.score} High score: {self.high_score}", align=ALIGMENT,font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
-        self.clear()
         self.score += 1
         self.update_scoreboard()
 
